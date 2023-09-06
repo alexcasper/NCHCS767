d9747798165cdf0287d3177dcf25f7728e42c087
alxcspr <not@territory.cc>
2023-09-06 15:55:39 +0100

    they do not

--- showtime.js
+++ showtime.js
@@ -20,7 +20,7 @@ function actionFile(filename, targetFilename) {
     fs.readFile(filename, { 'encoding': 'utf8' }, async (err, txt) => {
         if (err) { console.log(err); } else { 
             writeFile(targetFilename, await replaceEnv(txt,'GITHUB_VALUE_'))
-            let canvasTitle = targetFilename.split(['/']).slice(5,7).join('/').split('.')[0]
+            let canvasTitle = targetFilename.split(['/']).slice(5,7).join('-').split('.')[0]
             writeToCanvas(canvasTitle,converter.makeHtml(await replaceEnv(txt,'CANVAS_COURSE_')))
         }
     })
